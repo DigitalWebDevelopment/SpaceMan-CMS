828fe5216d87bbfdea3fc68f1c6dfe922abf0c07
Garrick Stace <master@FraggedAgain>
2017-04-15 16:28:07 +0800

    Add log file settings

--- index.php
+++ index.php
@@ -17,13 +17,23 @@ require_once APP_ROOT.AUTOLOAD.DS.'psr4autoloader.php';
 use digiwebdev\spaceman\application\SpaceMan_CMS as Spaceman;
 use digiwebdev\spaceman\config\Config as Config;
 
+/*
+ * Use this to set the evironment of the appliaction
+ * 
+ * 'DEV'     - for development and testing (Displays errors and warnings)
+ * 'DEV_ERR' - for development and testing (Displays only errors)
+ * 'PROD'    - for deployment of production application (Suppresses errors and
+ * warnings)
+ */
+Config::set('environment','DEV');
 
-// Use this to set the evironment of the appliaction
-// 
-// 'DEV'     - for development and testing (Displays errors and warnings)
-// 'DEV_ERR' - for development and testing (Displays only errors)
-// 'PROD'    - for deployment of production application (Suppresses errors and warnings)
-#Config::set('environment','DEV');
+/*
+ * Use this to set log file destination
+ * 
+ * 'spaceman' - Default (Application)
+ * 'syslog'   - System log file (Linux)
+ */
+Config::set('log_file', 'spaceman');
 
 // Start application
 #App::start('init');
